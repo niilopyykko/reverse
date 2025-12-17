592c69fdae07ecd9ce878826c8bcf7d22d874893
Niilo <niilo.pyykko@student.lut.fi>
2025-12-17 15:02:57 +0200

    Fix input handling and file operations in reverse.c for correct argument usage

--- reverse.c
+++ reverse.c
@@ -9,26 +9,28 @@ int main(int argc, char *argv[])
     }
     else if (argc == 1)
     {
-        const char inputFile = *argv[0];
-
+        char *inputFile = argv[0];
         FILE *userInput = stdin;
+        fprintf(getline(userInput).reverse());
     }
 
     else if (argc == 2)
     {
-        const char outputFile = *argv[1];
+        char *inputFile = argv[0];
+
+        FILE *userInput = stdin;
+        readFile(userInput);
+        char *outputFile = argv[1];
+
+        // WRITE FILE
+        FILE *WriteFile;
+        WriteFile = fopen(outputFile, "w");
     }
 
     else
     {
         printf("wery broken c-code ah yes");
     }
-
-    // REVERSE LINES
-
-    // WRITE FILE
-    FILE *WriteFile;
-    WriteFile = fopen(outputFile, "w");
 
     exit(1);
 }
@@ -41,6 +43,8 @@ char readFile(inputFile)
 
     // READ FILE
     getline(&buffer, &size, stdin);
+
+    // REVERSE LINES
 
     return (0)
 }
