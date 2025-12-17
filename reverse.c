50500a6338e43ea0fff4d97dc2d13abd41211342
Niilo <niilo.pyykko@student.lut.fi>
2025-12-17 14:36:36 +0200

    Add initial implementation of reverse.c with file handling structure

---
+++ reverse.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc == 0)
+    {
+        FILE *userInput = stdin;
+    }
+    const char inputFile;
+    const char outputFile;
+
+    // OPEN FILE
+    FILE *readFile;
+    readFile = fopen(inputFile, "r");
+
+    // READ FILE
+
+    // REVERSE LINES
+
+    // WRITE FILE
+    FILE *WriteFile;
+    WriteFile = fopen(outputFile, "w");
+
+    exit(1);
+}
